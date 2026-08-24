c98cdc523cdf86ffd6b3559af82e1c7f8505af45
charan bhogavalli <charanbhogavalli04116@gmail.com>
2026-08-24 11:03:59 +0530

    Time: 3 ms (34.51%), Space: 62.8 MB (98.18%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -1,25 +1,21 @@
 class Solution {
 public:
     int maxArea(vector<int>& height) {
-    // initialization of pointers and max area variable
-     int l=0;
-     int r=height.size()-1;
-     int maxarea=0;
-    //  using pointers for finding best height and width container
-     while(l<=r){
-        int wid=(r-l);
-        int minheight=min(height[l],height[r]);
-        int area=wid*minheight;
-            maxarea=max(maxarea,area);
-        if(height[l]>height[r]){
-            r--;
+        int i=0;
+        int j=height.size()-1;
+        int maxarea=INT_MIN;
+        while(i<=j){
+            int heights=min(height[i],height[j]);
+            int width=j-i;
+            maxarea=max(heights*width,maxarea);
+            if(height[i]<height[j]){
+                i++;
+            }
+            else{
+                j--;
+            }
+            
         }
-        else{
-            l++;
-        }
-     
-     }
-    //  returning maxarea
-     return maxarea;
+      return maxarea;  
     }
-}; 
+};
