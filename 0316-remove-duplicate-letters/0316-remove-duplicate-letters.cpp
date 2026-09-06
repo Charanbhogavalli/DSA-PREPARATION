7503c12be92ba393e3a84866bf37440872df5bf0
charan bhogavalli <charanbhogavalli04116@gmail.com>
2026-09-06 23:54:55 +0530

    Time: 0 ms (100%), Space: 10 MB (6.48%) - LeetHub

---
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+        unordered_map<char,int> mp;
+        unordered_map<char,bool> checked;
+        stack<char> st;
+        for(int i=0;i<s.size();i++){ 
+            mp[s[i]]++; 
+        } 
+        for(int i=0;i<s.size();i++){
+            mp[s[i]]--;
+           if(checked[s[i]]){
+            continue;
+           }
+        while(!st.empty() && st.top()>s[i] && mp[st.top()]>0){
+             checked[st.top()]=false;
+             st.pop();
+        }
+        st.push(s[i]);
+        checked[s[i]]=true;
+                 
+        }
+        string ans;
+        while(!st.empty()){
+            char temp=st.top();
+            ans+=temp;
+            st.pop();
+        }
+        reverse(ans.begin(),ans.end());
+        if(ans.size()==0){
+            return "0";
+        }
+        return ans; 
+    }
+};
